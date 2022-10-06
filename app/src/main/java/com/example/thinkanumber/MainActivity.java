93b9f450e88d0ba2df72401edccb73b254facf67
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-06 14:31:03 +0200

    Add if else logic to plus and minus buttons

--- app/src/main/java/com/example/thinkanumber/MainActivity.java
+++ app/src/main/java/com/example/thinkanumber/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.thinkanumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button plusButton;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView pic3;
     private ImageView pic4;
     private int tipp;
+    private int gondolt;
+    private Random rng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +37,29 @@ public class MainActivity extends AppCompatActivity {
         plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(tipp < 10){
+                    tipp++;
+                    tippValue.setText(String.valueOf(tipp));
+                }else{
+                    Toast.makeText(MainActivity.this, "A tipp nem lehet 10-nél több", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(tipp > 1){
+                    tipp--;
+                    tippValue.setText(String.valueOf(tipp));
+                }else{
+                    Toast.makeText(MainActivity.this, "A tipp nem lehet 1-nél kisebb", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         tippButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                
             }
         });
 
@@ -58,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
         pic2=findViewById(R.id.Pic2);
         pic3=findViewById(R.id.Pic3);
         pic4=findViewById(R.id.Pic4);
+        rng = new Random();
         tipp = 1;
+        gondolt = rng.nextInt(10)+1;
+        Log.d("gondolt",String.valueOf(gondolt));
     }
 }
